6af641a69b01ef926770cf6a294ce0e83845a81d
Ally-3 <146335458+Ally-3@users.noreply.github.com>
2023-11-16 16:58:46 +0000

    books routes completed

--- src/books/bookcontroller.js
+++ src/books/bookcontroller.js
@@ -1,7 +1,6 @@
 const Book = require("./bookmodel");
 
-// CREATE BOOK
-
+//1. CREATE BOOK - POST - adds a book to the DB
 async function addBook(req, res) {
     try {
         const newBook = {
@@ -26,7 +25,7 @@ async function addBook(req, res) {
     }
 }
 
-//LIST BOOKS
+//2. LIST BOOKS - GET - gets all books 
 async function listAllBooks (req, res){
     try {
         const listOfBooks = await Book.findAll();
@@ -39,7 +38,7 @@ async function listAllBooks (req, res){
     }
 }
 
-//UPDATE PUBLISHER
+//3. UPDATE PUBLISHER - PUT - updates a book's publisher 
 async function updatePublisher(req, res){
     try {
         const updatepb = await Book.update(
@@ -59,9 +58,102 @@ async function updatePublisher(req, res){
     }
 }
 
-//DELETE BOOK
+//4. DELETE BOOK - DELETE - deletes a single book by title 
 async function deleteBook(req, res){
+    try {
+        const del = await Book.destroy({ 
+            where: { title: req.body.title }
+        });
 
+        res.status(200).json({
+            message: "book deleted",
+            book: del
+        });
+    } catch (error) {
+        res.status(501).json({ 
+            message: error.message, 
+            error: error
+        })
+    }
+}
+
+//5 - DELETE - deletes all books 
+async function deleteAllBooks(req, res){
+    try {
+        const delAll = await Book.destroy({
+            truncate : true
+        });
+
+        res.status(200).json({
+            message: "all books have been deleted",
+            book: delAll
+        });
+    } catch (error) {
+        res.status(501).json({ 
+            message: error.message, 
+            error: error
+        })
+    }
+}
+
+//6 - GET - gets a book by author 
+async function booksByAuthor (req, res){
+    try {
+        const findBooks = await Book.findAll({
+            where : {
+                author: req.body.author
+            }
+        });
+
+        res.status(200).json(findBooks);
+
+    } catch (error) {
+        res.status(501).json({ 
+            message: error.message, 
+            error: error
+        })
+    }
+}
+
+//7 - PUT - updates a book's genre
+async function updateGenre(req, res){
+    try {
+        const updategenre = await Book.update(
+            { genre: req.body.newGenre },
+            { where: { title: req.body.title } }
+        );
+
+        res.status(201).json({
+            message: "genre updated",
+            book: updategenre
+        });
+
+    } catch (error) {
+        res.status(501).json({ 
+            message: error.message, 
+            error: error
+        })
+    }
+}
+//8 - PUT - updates a book's author 
+async function updateAuthor(req, res){
+    try {
+        const updateauthor = await Book.update(
+            { author: req.body.newAuthor },
+            { where: { title: req.body.title } }
+        );
+
+        res.status(201).json({
+            message: "author updated",
+            book: updateauthor
+        });
+
+    } catch (error) {
+        res.status(501).json({ 
+            message: error.message, 
+            error: error
+        })
+    }
 }
 
 // EXPORTS
@@ -70,5 +162,9 @@ module.exports = {
     addBook,
     listAllBooks,
     updatePublisher,
-    deleteBook 
+    deleteBook,
+    deleteAllBooks,
+    booksByAuthor,
+    updateGenre,
+    updateAuthor 
 };
--- src/routes/bookroutes.js
+++ src/routes/bookroutes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const bookRouter = Router();
 
-const { addBook, listAllBooks, updatePublisher, deleteBook } = require("../books/bookcontroller");
+const { addBook, listAllBooks, updatePublisher, deleteBook, deleteAllBooks, booksByAuthor, updateGenre, updateAuthor } = require("../books/bookcontroller");
 
 //ROUTE 1 - CREATE BOOK - adds book to database
 bookRouter.post("/addbook", addBook)
@@ -15,6 +15,18 @@ bookRouter.put("/updatepublisher", updatePublisher)
 
 //ROUTE 4 - DELETE BOOK
 bookRouter.delete("/deletebook", deleteBook)
+
+//ROUTE 5 - DELETE - deletes all books 
+bookRouter.delete("/deleteallbooks", deleteAllBooks)
+
+//ROUTE 6 - GET - gets a book by author 
+bookRouter.get("/booksbyauthor", booksByAuthor)
+
+//ROUTE 7 - PUT - updates a book's genre
+bookRouter.put("/updategenre", updateGenre)
+
+//ROUTE 8 - PUT - updates a book's author 
+bookRouter.put("/updateauthor", updateAuthor)
 
 //EXPORT
 module.exports = bookRouter;
